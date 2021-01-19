b21db278bc95c5b1b7ad4dcda28c30308ead4ff2
Blandine Guillemot <blandine.guillemot@univ-rennes1.fr>
2021-01-19 21:48:06 +0100

    Correction test et ajout de Bootstrap

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -31,6 +31,7 @@ class Controller{
         echo "<!DOCTYPE html>";
 		echo "<html>";
 		echo "<head>";
+		echo "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1\" crossorigin=\"anonymous\">";
 		echo "<title>".$titre."</title>";
 		echo "</head>";
 
