13d2b099716da63aff2372bd357805e6bd6d7876
Blandine Guillemot <blandine.guillemot@univ-rennes1.fr>
2021-01-19 22:02:08 +0100

    Correction routeur et mise en forme

--- views/form_login.php
+++ views/form_login.php
@@ -1,14 +1,16 @@
  <?php /*if(!isset($_SESSION)){ session_start();}*/ ?>
 <?php /*if (isset($_SESSION['login']) && isset($_SESSION['password'])):*/ ?>
-	<p>Vous êtes déjà connecté</p>
+	<!-- <p>Vous êtes déjà connecté</p> -->
 <?php /*else:*/ ?>
-	<label for="login"> Login de l'utilisateur </label>
+<form>
+  <div class="mb-3">
+  	<label for="login"> Login de l'utilisateur </label>
 	<input type="input" name="login" /><br />
-
+</div>
+<div class="mb-3">
 	<label for="password"> Mot de passe </label>
 	<input type="password" name="password" /><br />
-
-	<input type="submit" class="btn btn-primary" name="submit" value="Connexion" />
-
-	</form>
+</div>
+<input type="submit" class="btn btn-primary" name="submit" value="Connexion" />
+</form>
 <?php /*endif*/ ?>
--- views/menu.php
+++ views/menu.php
@@ -1,5 +1,15 @@
 <h2>Menu</h2>
-<ul>
-	<li><a href="#">Bougies</a></li>
-	<li>...</li>
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Connexion</a>
+  </li>
 </ul>
